0bb9c3306dda2e25da636696d9f77e6075c0d654
Bhagyeshree Barhate <61777952+Bhagu12345@users.noreply.github.com>
2023-12-01 20:04:04 +0000

    datattpe summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+//1) Primitive DataTypes:-
+//String, Number, Boolean, null, undefined(variable decalre, memory space declare, waiting for value), Symbol(We want to make any value unquie), BigInt
+
+
+//2) Non-Primitive DataTypes (Refernce Type):-
+//Array, Objects, Functions
+
+const heros = ["bhagya", "upaa", "nidhi"];
+
+let myObje = {
+    name: "hitu",
+    age: 22,
+}
+
+const myFuncation = function() {
+    console.log("he");
+}
