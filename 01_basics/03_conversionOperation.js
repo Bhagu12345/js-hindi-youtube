d8db7533650f7c03d2ac88f85e3cc25fcc16c5c3
Bhagyeshree Barhate <61777952+Bhagu12345@users.noreply.github.com>
2023-12-01 18:39:42 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="33"
+console.log(typeof score);
+
+let numbertype = Number(score)
+console.log(typeof numbertype);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+let isLoggedIn1 = "1"
+let isLoggedIn2 = "true"
+let isLoggedIn3 = "hitesh"
+
+let xyz = Boolean(isLoggedIn1)
+console.log(xyz);
+
+// 1 => true; 0 => false
+// "" false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
