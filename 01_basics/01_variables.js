20b0cc8c157f9a35e18fc4c77ec9c384393581b1
Bhagyeshree Barhate <61777952+Bhagu12345@users.noreply.github.com>
2023-12-01 18:22:17 +0000

    DtatTypes and Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "Bhagya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword]);
+
+/*Prefer not be use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer version
+//number => 2 to power 53
+//bigint =>
+//string => ""
+//boolean => true/false
+//null => standalone value (typeof object)
+//undefined =>
+//symbol => unique
