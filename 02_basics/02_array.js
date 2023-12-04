3b63731132b56ee7bdef9491b403ce68074fcf62
Bhagyeshree Barhate <61777952+Bhagu12345@users.noreply.github.com>
2023-12-04 12:54:40 +0000

    spared array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,4 +1,12 @@
 const marval = ["Bhagya", "Upsi", "Nidhu"]
 const fri = ["Prah", "Bhagya", "Shubh"]
-marval.push(fri)
-console.log(marval);
+//marval.push(fri)
+//console.log(marval);
+
+const allAlpha = marval.concat(fri)
+//console.log(allAlpha);
+
+// ======== Spread Array=========
+
+const all_new_fri = [...marval, ...fri]
+console.log(all_new_fri);
