90e9852310e43f7c2dc5bfa0a790d87dd9bd094d
Bhagyeshree Barhate <61777952+Bhagu12345@users.noreply.github.com>
2023-12-12 11:40:19 +0000

    contol flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+const temp = 41
+if (temo = 41) {
+    console.log("Yes")
+} else {
+    console.log("No")
+}
+
+//neasted if else
+const balance = 1000
+if (balance < 500) {
+    console.log("less than 500");
+} else if(balance < 700) {
+    console.log("less than 700");
+
+}else if(balance < 900){
+    console.log("less than 900");
+}else{
+    console.log("less than 1200");
+
+}
+
+const userlogin = true
+const debitcard = true
+if(userlogin && debitcard){
+    console.log("Allow Access");
+}
+
+// && ||
